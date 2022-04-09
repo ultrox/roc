8b04f8b75d40cea7b7b8d9f642e05da143aa8d31
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2022-04-09 11:27:15 +0200

    replaced libc stuff for wasm

--- examples/gui/platform/src/graphics/shaders/quad.wgsl
+++ examples/gui/platform/src/graphics/shaders/quad.wgsl
@@ -4,31 +4,30 @@ struct Globals {
     ortho: mat4x4<f32>;
 };
 
-@group(0)
-@binding(0)
+[[group(0), binding(0)]]
 var<uniform> globals: Globals;
 
 struct VertexInput {
-    @location(0) position: vec2<f32>;
+    [[location(0)]] position: vec2<f32>;
 };
 
 struct Quad {
-    @location(1) pos: vec2<f32>; // can't use the name "position" twice for compatibility with metal on MacOS
-    @location(2) width: f32;
-    @location(3) height: f32;
-    @location(4) color: vec4<f32>;
-    @location(5) border_color: vec4<f32>;
-    @location(6) border_width: f32;
+    [[location(1)]] pos: vec2<f32>; // can't use the name "position" twice for compatibility with metal on MacOS
+    [[location(2)]] width: f32;
+    [[location(3)]] height: f32;
+    [[location(4)]] color: vec4<f32>;
+    [[location(5)]] border_color: vec4<f32>;
+    [[location(6)]] border_width: f32;
 };
 
 struct VertexOutput {
-    @builtin(position) position: vec4<f32>;
-    @location(0) color: vec4<f32>;
-    @location(1) border_color: vec4<f32>;
-    @location(2) border_width: f32;
+    [[builtin(position)]] position: vec4<f32>;
+    [[location(0)]] color: vec4<f32>;
+    [[location(1)]] border_color: vec4<f32>;
+    [[location(2)]] border_width: f32;
 };
 
-@stage(vertex)
+[[stage(vertex)]]
 fn vs_main(
     input: VertexInput,
     quad: Quad
@@ -52,9 +51,9 @@ fn vs_main(
 }
 
 
-@stage(fragment)
+[[stage(fragment)]]
 fn fs_main(
     input: VertexOutput
-) -> @location(0) vec4<f32> {
+) -> [[location(0)]] vec4<f32> {
     return input.color;
 }
--- examples/gui/platform/src/roc.rs
+++ examples/gui/platform/src/roc.rs
@@ -4,24 +4,33 @@ use core::mem::{self, ManuallyDrop};
 use roc_std::{ReferenceCount, RocList, RocStr};
 use std::ffi::CStr;
 use std::os::raw::c_char;
+use std::alloc::{alloc, realloc, Layout};
 
 #[no_mangle]
-pub unsafe extern "C" fn roc_alloc(size: usize, _alignment: u32) -> *mut c_void {
-    return libc::malloc(size);
+pub unsafe extern "C" fn roc_alloc(size: usize, alignment: u32) -> *mut c_void {
+    let layout = Layout::from_size_align(size, alignment as usize).unwrap();
+    let ptr = alloc(layout);
+
+    return ptr as *mut c_void;
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn roc_realloc(
     c_ptr: *mut c_void,
     new_size: usize,
-    _old_size: usize,
-    _alignment: u32,
+    old_size: usize,
+    alignment: u32,
 ) -> *mut c_void {
-    return libc::realloc(c_ptr, new_size);
+    let layout = Layout::from_size_align(old_size, alignment as usize).unwrap();
+    let ptr = realloc(c_ptr as *mut u8, layout, new_size);
+
+    return ptr as *mut c_void;
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn roc_dealloc(c_ptr: *mut c_void, _alignment: u32) {
+    // TODO free memory for wasm. 
+    #![cfg(not(target_arch="wasm32"))]
     return libc::free(c_ptr);
 }
 
@@ -40,12 +49,25 @@ pub unsafe extern "C" fn roc_panic(c_ptr: *mut c_void, tag_id: u32) {
 
 #[no_mangle]
 pub unsafe extern "C" fn roc_memcpy(dst: *mut c_void, src: *mut c_void, n: usize) -> *mut c_void {
-    libc::memcpy(dst, src, n)
+    let dst_bytes: *mut u8 = std::mem::transmute(dst);
+    let src_bytes: *mut u8 = std::mem::transmute(src);
+    let size = n as isize;
+    for offset in 0..size {
+        *dst_bytes.offset(offset) = *src_bytes.offset(offset);
+    }
+    dst
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn roc_memset(dst: *mut c_void, c: i32, n: usize) -> *mut c_void {
-    libc::memset(dst, c, n)
+    let dst_bytes = dst as *mut u8;
+    let size = n as isize;
+    let c8 = c as u8;
+    for offset in 0..size {
+        *dst_bytes.offset(offset) = c8;
+    }
+
+    dst
 }
 
 #[repr(transparent)]
